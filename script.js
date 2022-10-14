b11266b2b363e6f6c43f976d21abe68581d133a9
Parthib <parthibdhal11@gmail.com>
2022-10-15 01:58:12 +0530

    Added keyboar funtionality of Backspace and Delete key

--- script.js
+++ script.js
@@ -63,5 +63,16 @@ function keypress_event(e) {
 
     //added the enter key as well to get the result
     else if ('Enter' === e.key) compute(1);
-  
+       //backspace for removing the last input
+       if (e.key === "Backspace") {
+        var resultInput = valEl.innerText;
+        //remove the last element in the string
+        valEl.innerText = resultInput.substring(0, valEl.innerText.length - 1);
+      }     
+         // Delete key for removing all the input
+    if (e.key === "Delete") {
+        var resultInput = valEl.innerText;
+        //remove all elements in the string
+        valEl.innerText = 0
+      }
 }
